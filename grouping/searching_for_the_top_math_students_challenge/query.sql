0c6e999c9dbe10526a97e3dc52701594a403574d
pdogra98 <>
2025-11-18 17:37:48 +0000

    Searching for the Top Math Students Challenge

---
+++ grouping/searching_for_the_top_math_students_challenge/query.sql
@@ -0,0 +1,8 @@
+SELECT student_surname, 
+    AVG(grade) AS average_grade
+FROM student_grades
+WHERE subject_name = 'Mathematics'
+GROUP BY student_surname
+HAVING MAX(grade) >= 90
+ORDER BY average_grade desc
+LIMIT 10
